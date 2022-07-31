bc83982a0d4995bf3ec41a330024f7df0b195ac4
Gaz <dianghazy@protonmail.com>
2022-07-31 15:48:10 +0700

    Gaz:Gaz:

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="dark">
+
+<h1 class="text-3xl font-bold underline text-gray-500 dark:text-red-600">
+  Hello world!
+</h1>
+</div>
