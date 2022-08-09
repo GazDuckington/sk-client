ba1464ab6643923e0becbee8375e6e06de9f04fa
Gaz <dianghazy@protonmail.com>
2022-08-10 01:33:27 +0700

    Gaz: update 8 files and create 4 files

---
+++ src/elements/InputData.svelte
@@ -0,0 +1,9 @@
+<div>
+	<input />
+</div>
+
+<style lang="postcss">
+	div {
+		@apply w-full h-full mb-2 rounded-md;
+	}
+</style>
---
+++ src/elements/Navbar.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import ThemeSwitcher from '$elements/ThemeSwitcher.svelte';
+</script>
+
+<div class="nav flex flex-row p-1 rounded-b-md justify-between">
+	<ThemeSwitcher />
+</div>
+
+<style lang="postcss" global>
+	.nav {
+		@apply bg-gray-900 dark:bg-gray-100;
+	}
+</style>
