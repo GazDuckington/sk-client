1dae0d77e98463da99b1d493ffb1b8677f9ca46e
Gaz <dianghazy@protonmail.com>
2022-08-10 20:51:43 +0700

    up

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
-
+const dev = process.env.NODE_ENV === 'development';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -13,11 +13,14 @@ const config = {
 		adapter: adapter({
 			pages: 'docs',
 			assets: 'docs',
-			fallback: null
+			fallback: true
 		}),
-		prerender: {
-			default: true
+		paths: {
+			base: dev ? '' : '/sk-client'
 		},
+		// prerender: {
+		// 	default: true
+		// },
 		alias: {
 			$components: 'src/components',
 			$elements: 'src/elements',
